ce3fa1c727d5c4eaac9d5823e3b2cfd7e5d92937
niazi90 <niazi7589@gmail.com>
2025-08-07 14:19:50 +0500

    password generated

--- Qno9.dart
+++ Qno9.dart
@@ -1,1 +1,17 @@
 // Implement Dart code to generate a random password of a given length using a while loop.
+
+import 'dart:math';
+void main() {
+  int length =8;
+  String characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#\$%^&*()';
+  String password = '';
+  
+  Random random = Random();
+  
+  while (password.length < length) {
+    int index = random.nextInt(characters.length);
+    password += characters[index];
+  }
+  
+  print('Generated password: $password');
+}
